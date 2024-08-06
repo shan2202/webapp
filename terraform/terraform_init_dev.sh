4e58da96a4f639fa9f16d5716a050f2a544943c8
Shankar <47175846+shan2202@users.noreply.github.com>
2024-08-06 17:23:14 +0800

    Scb task (#3)

--- terraform/terraform_init_dev.sh
+++ terraform/terraform_init_dev.sh
@@ -11,4 +11,4 @@ DDBTABLENAME="tfstate-table"
 PROJECT_NAME="scb"
 
 terraform init -upgrade -reconfigure -backend=true -backend-config="bucket=$REGION-$BUCKETNAME" -backend-config="key=$PROJECT_NAME/terraform.tfstate" -backend-config="region=$REGION" -backend-config="dynamodb_table=$REGION-$DDBTABLENAME"
-terraform workspace select dev
+terraform workspace select -or-create dev
